be9dbd8b566baf23bb2bf7d8e74589394072200f
carmarfee <3073640166@qq.com>
2025-03-11 20:55:26 +0800

    log&&timer

--- code/timer/heaptimer.cpp
+++ code/timer/heaptimer.cpp
@@ -19,3 +19,88 @@ heaptimer::heaptimer(/* args */)
 heaptimer::~heaptimer()
 {
 }
+
+void HeapTimer::add(int id, int timeout, const TimeoutCallBack& cb) {
+    assert(id >= 0);
+    size_t i;
+    if(pos_.count(id) == 0) {
+        /* 新节点：堆尾插入，调整堆 */
+        i = heap_.size();
+        pos_[id] = i;
+        heap_.push_back({id, Clock::now() + ms(timeout), cb});
+        siftup_(i);
+    } 
+    else {
+        /* 已有结点：调整堆 */
+        i = pos_[id];
+        heap_[i].expires = Clock::now() + ms(timeout);
+        heap_[i].cb = cb;
+        if(!siftdown_(i, heap_.size())) {
+            siftup_(i);
+        }
+    }
+}
+
+bool HeapTimer::siftdown_(size_t index, size_t n) {
+    assert(index >= 0 && index < heap_.size());
+    assert(n >= 0 && n <= heap_.size());
+    size_t i = index;
+    size_t j = i * 2 + 1;
+    while(j < n) {
+        if(j + 1 < n && heap_[j + 1] < heap_[j]) j++;
+        if(heap_[i] < heap_[j]) break;
+        SwapNode_(i, j);
+        i = j;
+        j = i * 2 + 1;
+    }
+    return i > index;
+}
+
+void HeapTimer::siftup_(size_t i) {
+    assert(i >= 0 && i < heap_.size());
+    size_t j = (i - 1) / 2;
+    while(j >= 0) {
+        if(heap_[j] < heap_[i]) { break; }
+        SwapNode_(i, j);
+        i = j;
+        j = (i - 1) / 2;
+    }
+}
+
+void HeapTimer::del_(size_t index) {
+    /* 删除指定位置的结点 */
+    assert(!heap_.empty() && index >= 0 && index < heap_.size());
+    /* 将要删除的结点换到队尾，然后调整堆 */
+    size_t i = index;
+    size_t n = heap_.size() - 1;
+    assert(i <= n);
+    if(i < n) {
+        SwapNode_(i, n);
+        if(!siftdown_(i, n)) {
+            siftup_(i);
+        }
+    }
+    /* 队尾元素删除 */
+    pos_.erase(heap_.back().id);
+    heap_.pop_back();
+}
+
+void HeapTimer::tick() {
+    /* 清除超时结点 */
+    if(heap_.empty()) {
+        return;
+    }
+    while(!heap_.empty()) {
+        TimerNode node = heap_.front();
+        if(std::chrono::duration_cast<ms>(node.expires - Clock::now()).count() > 0) { 
+            break; 
+        }
+        node.cb();
+        pop();
+    }
+}
+
+void HeapTimer::pop() {
+    assert(!heap_.empty());
+    del_(0);
+}
--- code/timer/heaptimer.h
+++ code/timer/heaptimer.h
@@ -1,31 +1,62 @@
 /**
  * @file heaptimer.h
  * @author carmarfee (3073640166@qq.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2025-03-03
- * 
+ *
  * @copyright Copyright (c) 2025
- * 
+ *
  */
 
 #ifndef __HEAPTIMER_H__
 #define __HEAPTIMER_H__
 
-class timer {
+#include <queue>
+#include <unordered_map>
+#include <time.h>
+#include <algorithm>
+#include <arpa/inet.h>
+#include <functional>
+#include <assert.h>
+#include <chrono>
+#include "../log/log.h"
 
+typedef std::function<void()> TimeoutCallBack;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds ms;
+typedef Clock::time_point timepoint;
+
+class timer
+{
+public:
+    int id;
+    timepoint expires;
+    TimeoutCallBack cb;
+    bool operator<(const timer &t)
+    {
+        return expires < t.expires;
+    }
 };
 
-class heaptimer {
+class heaptimer
+{
 public:
-    heaptimer();
+    heaptimer() { heap_.reserve(64); };
     ~heaptimer();
+
+    void add(int id, int timeout, const TimeoutCallBack &cb);
+    void pop();
+    void tick();
 
 private:
     // members
+    void del_(size_t index);
+    void siftup_(size_t i);
+    void siftdown_(size_t index, size_t n);
+
+    std::vector<timer> heap_;
+    std::unordered_map<int, size_t> pos_;
 };
-
-
-
 
 #endif // __HEAPTIMER_H__
